ac2359b327864ed8849db90642825d781ce8f75b
Silvan Knecht <knecht.silvan@gmail.com>
2019-05-02 11:00:09 +0200

    bugfix

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use("/", express.static("public"));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname + "/public/html/index.html"))
 );

75d60c9128342b1a0dad9e18f76baaa621fdcae4
silviojaeger <silvio.jaeger@hotmail.com>
2019-05-17 14:48:35 +0200

    frontend graphs added

--- server/public/javascript/index.js
+++ server/public/javascript/index.js
@@ -17,10 +17,9 @@ function onMessage(topic, message) {
 //-----------------------------------------------------------------------------------------------
 
 //---Plotly chart--------------------------------------------------------------------------------
-CHART = document.getElementById("chart");
 
 //layout the plot
-var layout = {
+var tempLayout = {
   colorway: [
     "#86C232",
     "#86C232",
@@ -51,10 +50,74 @@ var layout = {
     linecolor: "white"
   }
 };
+
+var co2Layout = {
+  colorway: [
+    "#86C232",
+    "#86C232",
+    "#222629",
+    "#474B4F",
+    "#6B6E70",
+    "#6B6E70",
+    "#6B6E70"
+  ],
+  plot_bgcolor: "#474B4F",
+  paper_bgcolor: "#474B4F",
+  title: {
+    text: "CO2",
+    font: {
+      family: "Roboto Condensed, sans-serif",
+      size: 24,
+      color: "white"
+    }
+  },
+  xaxis: {
+    color: "white",
+    showline: true,
+    linecolor: "white"
+  },
+  yaxis: {
+    color: "white",
+    showline: true,
+    linecolor: "white"
+  }
+};
+
+var luftfLayout = {
+  colorway: [
+    "#86C232",
+    "#86C232",
+    "#222629",
+    "#474B4F",
+    "#6B6E70",
+    "#6B6E70",
+    "#6B6E70"
+  ],
+  plot_bgcolor: "#474B4F",
+  paper_bgcolor: "#474B4F",
+  title: {
+    text: "Luftfeuchtigkeit",
+    font: {
+      family: "Roboto Condensed, sans-serif",
+      size: 24,
+      color: "white"
+    }
+  },
+  xaxis: {
+    color: "white",
+    showline: true,
+    linecolor: "white"
+  },
+  yaxis: {
+    color: "white",
+    showline: true,
+    linecolor: "white"
+  }
+};
 //the actual data to plot
-var x = [1, 2, 3, 4, 5];
-var y = [0, 20, 100, 40, 50];
-var data= [
+var x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var y = [0, 20, 100, 40, 50, 60, 34, 56, 23, 2];
+var data = [
   {
     x: x,
     y: y
@@ -62,29 +125,68 @@ var data= [
 ];
 
 //make the plot
-Plotly.plot(
-  CHART,
-  data,
-  layout
-);
+Plotly.plot(document.getElementById("tempPlot"), data, tempLayout);
+Plotly.plot(document.getElementById("co2Plot"), data, co2Layout);
+Plotly.plot(document.getElementById("luftfPlot"), data, luftfLayout);
+//Plotly.update(CHART, data, layout);
+
 //------------------------------------------------------------------------------------------------
 
 //---Own Functions--------------------------------------------------------------------------------
 function flipIt() {
   document.getElementById("settings").classList.toggle("flipRigth");
-  document.getElementById("chart").classList.toggle("flipLeft");
+  document.getElementById("tempPlot").classList.toggle("flipLeft");
+  document.getElementById("co2Plot").classList.toggle("flipLeft");
+  document.getElementById("luftfPlot").classList.toggle("flipLeft");
 }
 
-var slider = document.getElementById("slider");
-slider.oninput = function() {
-  updateSlider();
+function checkPlots() {
+  var boxes = document.querySelectorAll('#plotBoxes > input');
+  for (var i = 0; i < boxes.length; i++) {
+    if(boxes[i].checked){
+      addChart(boxes[i].value)
+    }else{
+      removeChart(boxes[i].value)
+    }
+  }
 }
 
-
-function updateSlider(){
-  var i = slider.value;
-  y[4]=Number(i);
-  console.log(x);
-  Plotly.update(CHART, data, layout);
+function checkGraphs(){
+  var graphs = document.querySelectorAll('#graphBoxes > input');
+  for (var i = 0; i < graphs.length; i++) {
+    if(graphs[i].checked){
+      addGraph(graphs[i].value)
+    }else{
+      removeGraph(graphs[i].value)
+    }
+  }
 }
 //-----------------------------------------------------------------------------------------------
+//SHOW OR HIDE PLOTS
+function addChart(chart) {
+  console.log("add Chart: " + chart);
+  document.getElementById(chart).style.display = 'block';
+}
+function removeChart(chart) {
+  document.getElementById(chart).style.display = 'none';
+}
+
+function addGraph(graph){
+  console.log("add Graph: " + graph);
+}
+
+function removeGraph(graph){
+  console.log("remove Graph: " + graph);
+}
+
+//UPDATE PLOTS
+function updatePlot(chart){
+  Plotly.update(document.getElementById(chart), data, tempLayout);
+}
+
+var counter = 0;
+window.setInterval(function(){
+  counter+=0.05;
+  y[4]= Math.sin(counter)*50+50;
+  updatePlot('tempPlot');
+}, 10);
